0c5ec7357c3aace8e4b46ff8bdd6c9d0452e0107
Veysel GORGEN <92218276+vgorgen@users.noreply.github.com>
2023-09-04 14:08:11 +0300

    InputSSystem reference deleted from ShipMovementScript

--- Assets/Scripts/Player Scripts/ShipMovementScript.cs
+++ Assets/Scripts/Player Scripts/ShipMovementScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
