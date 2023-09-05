56c29f4353e2b3fed9abdb749a6babe485de7fa9
ERENSISMAN61 <eren_sisman20@erdogan.edu.tr>
2023-09-05 15:51:28 +0300

    unity version upgraded

--- Assets/Scripts/Player Scripts/Floater.cs
+++ Assets/Scripts/Player Scripts/Floater.cs
@@ -13,7 +13,7 @@ public class Floater : MonoBehaviour
     public float waterDrag = 2f;
     public float waterAngularDrag = 2f;
     public WaterSurface water;
-    WaterSearchParameters search = new WaterSearchParameters();
+    //WaterSearchParameters search = new WaterSearchParameters();
     WaterSearchResult searchResult;
     private void FixedUpdate()
     {
@@ -22,24 +22,24 @@ public class Floater : MonoBehaviour
 
         // Create water search parameters and get the water surface height.
         
-        search.startPosition = transform.position;
+        //search.startPosition = transform.position;
 
         
-        water.FindWaterSurfaceHeight(search, out searchResult);
+        //water.FindWaterSurfaceHeight(search, out searchResult);
 
-        // Check if the object is below the water surface.
-        if (transform.position.y < searchResult.height)
-        {
-            // Calculate the displacement multiplier based on depth.
-            float displacementMultiplier = Mathf.Clamp01((searchResult.height - transform.position.y) / depthBeforeSubmerge) * displacementAmount;
+        //// Check if the object is below the water surface.
+        //if (transform.position.y < searchResult.height)
+        //{
+        //    // Calculate the displacement multiplier based on depth.
+        //    float displacementMultiplier = Mathf.Clamp01((searchResult.height - transform.position.y) / depthBeforeSubmerge) * displacementAmount;
 
-            // Apply buoyant force.
-            rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+        //    // Apply buoyant force.
+        //    rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
 
-            // Apply drag forces.
-            rb.AddForce(displacementMultiplier * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
-            rb.AddTorque(displacementMultiplier * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
-        }
+        //    // Apply drag forces.
+        //    rb.AddForce(displacementMultiplier * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        //    rb.AddTorque(displacementMultiplier * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        //}
 
         // Resetting the rotation might not be necessary, so you can remove this line.
         //gameObject.transform.rotation = Quaternion.identity;
