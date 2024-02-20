1877e4837507a94f4f844c7ef8a6fe6ce6ca1215
Veysel GORGEN <92218276+vgorgen@users.noreply.github.com>
2024-02-20 13:30:52 +0300

    Fixed Player Fire script
    When you clicked left with mouse for shoot you would click the objects but now you can shoot where you want
    To do list:
    You will fix the red enemy turning mechanics(it turns sometimes fast sometimes slow)

--- Assets/Scripts/Player Scripts/PlayerFire.cs
+++ Assets/Scripts/Player Scripts/PlayerFire.cs
@@ -24,10 +24,6 @@ public class PlayerFire : MonoBehaviour
     public CinemachineFreeLook leftCamera;
     private GameObject cam;
 
-    public Vector3 offset_FirePos0;
-    public Vector3 offset_FirePos1;
-    public Vector3 offset_FirePos2;
-
 
     private GameObject rightTrajectory;
     private GameObject leftTrajectory;
@@ -54,7 +50,7 @@ public class PlayerFire : MonoBehaviour
         //drawTrajectory = gameObject.GetComponentInChildren<DrawTrajectory>();
         mousePosition = GameObject.FindGameObjectWithTag("MousePosition").GetComponent<Right_Left_Aim>();
         ammo = maxAmmo; // Mermi sayısını maksimum mermi sayısına eşitle
-       
+
         rightTrajectory = GameObject.FindGameObjectWithTag("RightTrajectory");
         leftTrajectory = GameObject.FindGameObjectWithTag("LeftTrajectory");
         cam = GameObject.FindGameObjectWithTag("MainCamera");
@@ -62,31 +58,24 @@ public class PlayerFire : MonoBehaviour
     }
 
 
-    void FireCannonball(Vector3 targetPoint ,Transform firePoint, Vector3 offset)
+    void FireCannonball(Vector3 targetPoint, Transform firePoint)
     {
 
-        Vector3 firePointRotation = (targetPoint - firePoint.position) + offset;
+        //Vector3 firePointRotation = (targetPoint - firePoint.position);
 
-        // Topun rotasyonunu, hedefe doğru bakacak şekilde ayarla
-        Quaternion fireRotation = Quaternion.FromToRotation(firePoint.forward, firePointRotation);
+        //// Topun rotasyonunu, hedefe doğru bakacak şekilde ayarla
+        //Quaternion fireRotation = Quaternion.FromToRotation(firePoint.forward, firePointRotation);
 
         GameObject cannonball = Instantiate(cannonballPrefab, firePoint.position, Quaternion.identity);
         Rigidbody rb = cannonball.GetComponent<Rigidbody>();
-        
-
-        //float radianAngle = Mathf.Deg2Rad * launchAngle;
 
 
-        ////drawProjection.ShowTrajectory(firePoint.position, initialSpeed, launchAngle);
-        //Vector3 mouseInput = rightCamera.m_LookAt.position;
+        Ray ray = cam.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
 
-        //Vector3 launchDirection = firePoint.forward * 0.5f * Mathf.Abs(Physics.gravity.y) * 5 * 5;
-        
+        Debug.Log("Ray: " + ray.direction * 100f);
+        rb.AddForce((ray.direction * initialSpeed) + (Vector3.up * launchAngle), ForceMode.Impulse);
 
 
-        rb.velocity = (firePointRotation.normalized) * initialSpeed;
-
-       
         Destroy(cannonball, 5f);
         lastShotTime = Time.time;
 
@@ -103,8 +92,6 @@ public class PlayerFire : MonoBehaviour
     {
         if (Input.GetMouseButton(1))
         {
-            Ray ray = cam.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
             if (Input.GetMouseButtonDown(0))
             {
                 // Mermi sayısının sıfırdan büyük olduğunu kontrol et
@@ -112,36 +99,30 @@ public class PlayerFire : MonoBehaviour
                 {
                     if (Time.time - lastShotTime > timeBetweenShots)
                     {
-                       
-                        // Draw the ray in the Scene view
-                        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.green, 2f);
+
+                        
                         if (rightCamera.Priority > leftCamera.Priority)
                         {
 
                             if (isActiveLevel1)
                             {
-                                
 
-                                if (Physics.Raycast(ray, out hit, Mathf.Infinity, targetLayer))
-                                {
-                                    FireCannonball(hit.point, rightFirePoint[0], Vector3.right * 5);
-                                }
+                                FireCannonball(Vector3.forward, rightFirePoint[0]);//Vector3.forward is not important here you will fix the parameter later
+
                             }
 
                             if (isActiveLevel2)
                             {
-                                if (Physics.Raycast(ray, out hit, Mathf.Infinity, targetLayer))
-                                {
-                                    FireCannonball(hit.point, rightFirePoint[1], offset_FirePos1);
-                                }
+
+                                FireCannonball(Vector3.forward, rightFirePoint[1]);
+
                             }
 
                             if (isActiveLevel3)
                             {
-                                if (Physics.Raycast(ray, out hit, Mathf.Infinity, targetLayer))
-                                {
-                                    FireCannonball(hit.point, rightFirePoint[2], Vector3.left * 5);
-                                }
+
+                                FireCannonball(Vector3.forward, rightFirePoint[2]);
+
                             }
 
                             lastShotTime = Time.time;
@@ -151,26 +132,23 @@ public class PlayerFire : MonoBehaviour
                         {
                             if (isActiveLevel1)
                             {
-                                if (Physics.Raycast(ray, out hit, Mathf.Infinity, targetLayer))
-                                {
-                                    FireCannonball(hit.point, leftFirePoint[0], Vector3.right * 5);
-                                }
+
+                                FireCannonball(Vector3.forward, leftFirePoint[0]);
+
                             }
 
                             if (isActiveLevel2)
                             {
-                                if (Physics.Raycast(ray, out hit, Mathf.Infinity, targetLayer))
-                                {
-                                    FireCannonball(hit.point, leftFirePoint[1], offset_FirePos1);
-                                }
+
+                                FireCannonball(Vector3.forward, leftFirePoint[1]);
+
                             }
 
                             if (isActiveLevel3)
                             {
-                                if (Physics.Raycast(ray, out hit, Mathf.Infinity, targetLayer))
-                                {
-                                    FireCannonball(hit.point, leftFirePoint[2], Vector3.left * 5);
-                                }
+
+                                FireCannonball(Vector3.forward, leftFirePoint[2]);
+
                             }
                             lastShotTime = Time.time;
                         }
@@ -196,46 +174,10 @@ public class PlayerFire : MonoBehaviour
         //isReloading = false;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         FireFunction2();
-        //if (lineRenderer != null)
-        //{
-        //    if (Input.GetMouseButton(1))
-        //    {
-        //        VisualizeTrajectory();
-        //        lineRenderer.enabled = true;
-        //    }
-        //    if (Input.GetMouseButtonUp(1))
-        //    {
-        //        initialSpeed = 0f;
-        //        lineRenderer.enabled = false;
-        //    }
-        //}
-
-        //float mouseY = Input.GetAxis("Mouse Y");
-
-        //if (mouseY == 0f)
-        //{
-        //    // Fare Y ekseninde hareket etmiyor
-        //    // initialSpeed'i sabit tut
-        //}
-        //else
-        //{
-        //    // Fare Y ekseninde hareket ediyor
-        //    if (mouseY > 0f)
-        //    {
-        //        // Yukarı doğru hareket
-        //        initialSpeed += speedChangedRate * Time.deltaTime;
-        //    }
-        //    else
-        //    {
-        //        // Aşağı doğru hareket
-        //        initialSpeed -= speedChangedRate * Time.deltaTime;
-        //        initialSpeed = Mathf.Max(initialSpeed, 0f); // Minimum hız sınırlaması (sıfırdan küçük olamaz)
-        //    }
-        //}
-
+        
     }
 
 
