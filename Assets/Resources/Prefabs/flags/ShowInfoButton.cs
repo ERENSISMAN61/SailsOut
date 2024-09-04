c963e63dc49d7b9c7d3906ee4c5daee23ec02ce6
Veysel GORGEN <92218276+vgorgen@users.noreply.github.com>
2024-09-04 20:07:43 +0300

    Intro Scene Created

---
+++ Assets/Resources/Prefabs/flags/ShowInfoButton.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowInfoButton : MonoBehaviour
+{
+    public Button myButton;
+    public TextMeshProUGUI countryName;
+    // Action event tanımlama
+    public event Action onButtonClicked;
+    public bool isClickedShowInfo;
+
+    [SerializeField]
+    private string CountryName;
+    private string CountryInfo;
+
+    void Start()
+    {
+        // Button'un onClick event'ine bir lambda fonksiyon ekleyin
+        myButton.onClick.AddListener(() => onButtonClicked?.Invoke());
+    }
+
+    void OnEnable()
+    {
+        // Dinlemek istediğiniz farklı fonksiyonları burada Action'a ekleyebilirsiniz
+        onButtonClicked += LogButtonClickedTrue;
+        onButtonClicked += PerformAnotherAction;
+    }
+
+    void OnDisable()
+    {
+        // Unsubscribe to avoid memory leaks
+        onButtonClicked -= LogButtonClickedFalse;
+        onButtonClicked -= PerformAnotherAction;
+    }
+
+    private void LogButtonClickedTrue()
+    {
+        Debug.Log("Button tıklandı!");
+        isClickedShowInfo = true;
+    }
+
+    private void LogButtonClickedFalse()
+    {
+        Debug.Log("Button tıklandı!");
+        isClickedShowInfo = true;
+    }
+
+    private void PerformAnotherAction()
+    {
+        Debug.Log("Başka bir işlem yapılıyor...");
+        countryName.text = CountryName;
+    }
+}
---
+++ Assets/Resources/Prefabs/flags/showInfoAnim.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class showInfoAnim : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject showCountryInfoPanel;
+    public GameObject saydamPanel;
+    private int isClicked = 0;
+    
+
+    void Start()
+    {
+        showCountryInfoPanel.SetActive(false);
+        // Animator componentini al
+        animator = GetComponent<Animator>();
+
+        // Başlangıçta animasyonu durdur
+        animator.SetBool("isClicked", false);
+        animator.SetBool("isClicked2", false);
+    }
+
+    // Bu method butona bağlanacak
+    public void ToggleAnimation()
+    {
+        
+        // Animasyonun durumunu tersine çevir
+        animator.SetBool("isClicked", true);
+        animator.SetBool("isClicked2", false);
+        Invoke("showInfoActive", 2.5f);
+
+
+    }
+
+    public void ToggleAnimationOut()
+    {
+        // Mevcut durumu al
+        bool isCurrentlyPlaying = animator.GetBool("isClicked2");
+        showInfoPasive();
+        // Animasyonun durumunu tersine çevir
+        animator.SetBool("isClicked", false);
+        animator.SetBool("isClicked2", true);
+       
+    }
+
+    void OnButtonClick()
+    {
+        Debug.Log("Button tıklandı!");
+    }
+
+    private void showInfoActive()
+    {
+        showCountryInfoPanel.SetActive(true);
+    }
+
+    private void showInfoPasive()
+    {
+        showCountryInfoPanel.SetActive(false);
+    }
+}
